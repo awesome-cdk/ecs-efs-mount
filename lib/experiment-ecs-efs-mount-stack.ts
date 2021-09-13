79ec76c3c30c125ec7b2ba511eb6fbce064c015f
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2021-09-13 16:07:06 +0300

    feat: Micro Express app, served through AWS ECS Fargate

--- lib/experiment-ecs-efs-mount-stack.ts
+++ lib/experiment-ecs-efs-mount-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from '@aws-cdk/core';
 import * as ecs from '@aws-cdk/aws-ecs';
 import {ApplicationLoadBalancedFargateService} from "@aws-cdk/aws-ecs-patterns";
+import * as path from "path";
+import {Duration} from "@aws-cdk/core";
 
 export class ExperimentEcsEfsMountStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -10,15 +12,14 @@ export class ExperimentEcsEfsMountStack extends cdk.Stack {
 
         const loadBalancedFargateService = new ApplicationLoadBalancedFargateService(this, 'Service', {
             cluster,
-            memoryLimitMiB: 1024,
-            cpu: 512,
+            memoryLimitMiB: 512,
+            cpu: 256,
+            desiredCount: 2,
             taskImageOptions: {
-                image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+                image: ecs.ContainerImage.fromAsset(path.resolve(__dirname, './../app')),
+                containerPort: 8080,
             },
+            healthCheckGracePeriod: Duration.seconds(10),
         });
-
-        // loadBalancedFargateService.targetGroup.configureHealthCheck({
-        //     path: "/custom-health-path",
-        // });
     }
 }
