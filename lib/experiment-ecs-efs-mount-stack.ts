17324d5ce06933250852181d6bb899ff1bc77ad7
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2021-09-13 15:27:43 +0300

    chore: Initial commit

--- lib/experiment-ecs-efs-mount-stack.ts
+++ lib/experiment-ecs-efs-mount-stack.ts
@@ -1,9 +1,24 @@
 import * as cdk from '@aws-cdk/core';
+import * as ecs from '@aws-cdk/aws-ecs';
+import {ApplicationLoadBalancedFargateService} from "@aws-cdk/aws-ecs-patterns";
 
 export class ExperimentEcsEfsMountStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+        const cluster = new ecs.Cluster(this, 'Cluster');
+
+        const loadBalancedFargateService = new ApplicationLoadBalancedFargateService(this, 'Service', {
+            cluster,
+            memoryLimitMiB: 1024,
+            cpu: 512,
+            taskImageOptions: {
+                image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+            },
+        });
+
+        // loadBalancedFargateService.targetGroup.configureHealthCheck({
+        //     path: "/custom-health-path",
+        // });
+    }
 }
